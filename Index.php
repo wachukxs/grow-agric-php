245b34c1b0810adf42aa7f48501a7ff0f0393c82
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2021-09-07 14:30:13 +0100

    using safeload for .env and unsafe getenv

--- Index.php
+++ Index.php
@@ -1,9 +1,9 @@
 <?php
     require "vendor/autoload.php";
 
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
+    $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__); // https://github.com/vlucas/phpdotenv#putenv-and-getenv
+    $dotenv->safeLoad();
 
     echo "Welcome to Grow Agric's API! ";
-    echo 'If you can see this, reach us via ' . $_ENV["GROW_AGRIC_DEV_EMAIL"] . '!';
+    echo 'If you can see this, reach us via ' . getenv("GROW_AGRIC_DEV_EMAIL") . '!'; // echo 'If you can see this, reach us via ' . $_ENV["GROW_AGRIC_DEV_EMAIL"] . '!';
 ?>
