887e77474f46ed2e17cb6584915405a310e9925f
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2021-09-30 21:00:03 +0300

    update to dynamically use evn variables.

--- config/Database.php
+++ config/Database.php
@@ -1,6 +1,9 @@
 <?php
     require "../vendor/autoload.php";
 
+    $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__); // https://github.com/vlucas/phpdotenv#putenv-and-getenv
+    $dotenv->safeLoad();
+
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
@@ -15,10 +18,18 @@
         public function connect() // must be public [& not __construct, cause it'll return a Database data type not PDO], else we can't call it elsewhere
         {
             # should this block be in a constructor method or sth?
-            $this->database_host = $_ENV["GROW_AGRIC_HOST_NAME_LOCAL"]; // $_ENV["GROW_AGRIC_HOST_NAME"];
-            $this->database_name = $_ENV["GROW_AGRIC_DATABASE_NAME_TEST"]; // eventually dynamically set to prod/test
-            $this->database_username = $_ENV["GROW_AGRIC_DATABASE_USER_NAME_TEST"];
-            $this->database_password = $_ENV["GROW_AGRIC_DATABASE_PASSWORD_TEST"];
+            if (getenv("CURR_ENV") === "production") {
+                $this->database_host = $_ENV["GROW_AGRIC_HOST_NAME"];
+                $this->database_name = $_ENV["GROW_AGRIC_DATABASE_NAME_PROD"]; // eventually dynamically set to prod/test
+                $this->database_username = $_ENV["GROW_AGRIC_DATABASE_USER_NAME"];
+                $this->database_password = $_ENV["GROW_AGRIC_DATABASE_PASSWORD"];
+            } else {
+                $this->database_host = $_ENV["GROW_AGRIC_HOST_NAME_LOCAL"]; // $_ENV["GROW_AGRIC_HOST_NAME"];
+                $this->database_name = $_ENV["GROW_AGRIC_DATABASE_NAME_TEST"]; // eventually dynamically set to prod/test
+                $this->database_username = $_ENV["GROW_AGRIC_DATABASE_USER_NAME_TEST"];
+                $this->database_password = $_ENV["GROW_AGRIC_DATABASE_PASSWORD_TEST"];
+            }
+            
 
             $this->database_connection = null;
             try {
