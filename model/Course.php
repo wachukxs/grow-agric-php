696f9e3cb37b04cdeda2daa69dbbfc492c2f2d74
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2022-02-14 13:59:17 +0300

    query fix.

--- model/Course.php
+++ model/Course.php
@@ -364,11 +364,15 @@ class Course {
     public function getAllCompletedAndIncompletedCourses()
     {
         try {
-            $query = 'SELECT 
-            ANY_VALUE(lc.`mediatype`) AS mediatype, ANY_VALUE(lc.`name`) AS name, ANY_VALUE(lc.`description`) AS description, 
-            ANY_VALUE(lc.`id`) AS courseid, ANY_VALUE(lc.`url`) as url, ANY_VALUE(lc.`moduleid`) AS moduleid, 
-            IF(MAX(ll.`totalpages`) = MAX(ll.`currentpage`), true, false) AS completed, MAX(ll.end) AS last_time, 
-            ll.farmerid AS farmerid, ( SELECT currentpage FROM learning_info WHERE learning_info.end = MAX(ll.end) ) AS last_page 
+            $query = 'SELECT ANY_VALUE(lc.`mediatype`) AS mediatype, 
+            ANY_VALUE(lc.`name`) AS name, 
+            ANY_VALUE(lc.`description`) AS description, 
+            ANY_VALUE(lc.`id`) AS courseid, 
+            ANY_VALUE(lc.`url`) as url, 
+            ANY_VALUE(lc.`moduleid`) AS moduleid, 
+            IF(MAX(ll.`totalpages`) = MAX(ll.`currentpage`), true, false) AS completed, 
+            MAX(ll.end) AS last_time, ll.farmerid AS farmerid, 
+            ( SELECT currentpage FROM learning_info WHERE learning_info.end = MAX(ll.end) LIMIT 1 ) AS last_page 
             FROM learning_courses lc 
             LEFT JOIN learning_info ll 
             ON lc.id = ll.course_id 
