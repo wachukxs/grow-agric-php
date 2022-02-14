52c7d8983f28e8ce09eea1c4e542b841369d294a
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2022-02-14 11:26:07 +0300

    updates to fetch previous farm visit records.

---
+++ api/fieldagents/getfarmvisitrecords.php
@@ -0,0 +1,54 @@
+<?php
+
+// Headers
+// https://stackoverflow.com/a/17098221
+include_once '../../config/globals/header.php';
+
+// Resources
+include_once '../../config/Database.php';
+include_once '../../model/Records.php';
+include_once '../../model/Farmer.php';
+include_once '../../model/Farm.php';
+
+// Instantiate Database to get a connection
+$database_connection = new Database();
+$a_database_connection = $database_connection->connect();
+
+// Instantiate Records object
+$records = new Records($a_database_connection);
+$farmer = new Farmer($a_database_connection);
+$farm = new Farm($a_database_connection);
+$field_agents = new FieldAgents($a_database_connection);
+
+// get data
+$data = json_decode(file_get_contents('php://input'));
+/*
+
+
+*/
+file_put_contents('php://stderr', print_r("\n\n[===>] \n", TRUE));
+file_put_contents('php://stderr', print_r($data, TRUE));
+file_put_contents('php://stderr', print_r("\n\n[<===] \n", TRUE));
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET["fieldagentid"])) {
+
+        // getAllFieldAgentFarmVisitRecords
+        $result1 = $field_agents->getAllFieldAgentFarmVisitRecords($_GET["farmerid"]);
+        $row1["mortalities"] = $result1->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($row1);
+    } else {
+        file_put_contents('php://stderr', print_r("\n\n" . 'ERR: Trying to fetch previous farm visits for field agents, Bad data provided' . "\n", TRUE));
+
+        http_response_code(400);
+        echo json_encode(
+            array(
+                'message' => 'Bad data provided',
+                'response' => 'NOT OK',
+                'response_code' => http_response_code()
+            )
+        );
+    }
+    
+
+}
--- model/FieldAgents.php
+++ model/FieldAgents.php
@@ -12,6 +12,8 @@ class FieldAgents {
     {
         $this->database_connection = $a_database_connection;
     }
+
+
     public function getFieldAgentByEmail(string $_email)
     {
         try {
@@ -36,6 +38,35 @@ class FieldAgents {
         } catch (\Throwable $err) {
             //throw $err;
             file_put_contents('php://stderr', print_r('FieldAgents.php->getFieldAgentByEmail error: ' . $err->getMessage() . "\n", TRUE));
+            return false;
+        }
+    }
+
+    // change query to include fieldagent disignated counties:
+    public function getAllFieldAgentFarmVisitRecords($fieldagentid)
+    {
+        try {
+            // Create query
+            $query = 'SELECT * FROM fieldagents_farm_visits
+                WHERE
+                fieldagentid = :_fieldagentid
+            ';
+
+            // Prepare statement
+            $query_statement = $this->database_connection->prepare($query);
+
+            $faid = htmlspecialchars(strip_tags($fieldagentid));
+
+            // Execute query statement
+            $query_statement->bindParam(':_fieldagentid', $faid);
+
+            // Execute query statement
+            $query_statement->execute();
+
+            return $query_statement;
+        } catch (\Throwable $err) {
+            //throw $err;
+            file_put_contents('php://stderr', print_r('FieldAgents.php->getAllFieldAgentFarmVisitRecords error: ' . $err->getMessage() . "\n", TRUE));
             return false;
         }
     }
