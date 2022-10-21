e7e0b111968e5b2628193e1832763bb77e4142e9
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2022-10-21 15:53:53 +0300

    adding more todos, and reason for declining.

--- README.md
+++ README.md
@@ -2,28 +2,30 @@
 
 Watch me build great stuff.
 
-### API cababilities
+## API cababilities
 * Sign up a farmer.
 * Login a farmer.
+* A whole lot more ...
 
 
-### TODOs
-0. we should send an email for people that joined waiting list
-1. include a repititions in a file and include it, eg. when we set header config for api endpoints
+## TODOs
+* we should send an email for people that joined waiting list
+* include a repititions in a file and include it, eg. when we set header config for api endpoints
 
-2. check that phone number or email on sign up is not from delete account
-3. make sure we have these checks: if ($_SERVER["REQUEST_METHOD"] == "POST") and for other http methods too ... confirm what http method calls them in the frontend
-
-
-### Pitfalls
-1. Never use var_dump() ... it's like an api output
-
-To enable extenstion in heroku https://devcenter.heroku.com/changelog-items/514
-
-Maybe sth to look at https://packagist.org/packages/nicolab/php-ftp-client
+* check that phone number or email on sign up is not from delete account
+* make sure we have these checks: if ($_SERVER["REQUEST_METHOD"] == "POST") and for other http methods too ... confirm what http method calls them in the frontend
+* We should have if checks for POST and GET request to make sure we have all the data we need
+* Use php fun. to output name of file and fun. an error occurred in.
 
 
-sendmail_path = /usr/sbin/sendmail -t -i
+## Pitfalls
+* Never use var_dump() ... it's like an api output
+* To enable extenstion in heroku https://devcenter.heroku.com/changelog-items/514
+* Maybe sth to look at https://packagist.org/packages/nicolab/php-ftp-client
+
+
+## Some code snippets
+```sendmail_path = /usr/sbin/sendmail -t -i```
 
 ## Rules,
 
--- api/admin/finance-applications/updatestatus.php
+++ api/admin/finance-applications/updatestatus.php
@@ -24,27 +24,55 @@ $data = json_decode(file_get_contents('php://input'));
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try { // an if statement to check for 
 
-        $result = $finance->updateFinanceRegistrationStatus($data->lastupdateby, $data->status, $data->finance_application_id);
 
-        if ($result) {
-            $result1 = $finance->selectSingleFinanceRegistrationStatusByID($data->finance_application_id);
+        if (
+            isset($data->lastupdateby, $data->status, $data->finance_application_id)
+            &&
+            !empty($data->lastupdateby)
+            &&
+            !empty($data->status)
+            &&
+            !empty($data->finance_application_id)
+            && 
+            (
+                ($data->status == "APPROVED")
+                XOR
+                ($data->status == "DECLINED" && isset($data->reason) && !empty($data->reason))
+            )
+        ) {
+            $result = $finance->updateFinanceRegistrationStatus($data->lastupdateby, $data->status, $data->finance_application_id, $data->reason = NULL);
 
-            $row["updated_data"] = $result1->fetch(PDO::FETCH_ASSOC);
-
-            $result2 = $finance->getFarmerEmailAndFirstnameFromFinanceApplicationID($data->finance_application_id);
-            $farmerRow = $result2->fetch(PDO::FETCH_ASSOC);
-
-            file_put_contents('php://stderr', "\ndate of creation " . $farmerRow['created_on'] . "\n" . "\n", FILE_APPEND | LOCK_EX);
-
-            // send email
-            if (getenv("CURR_ENV") == "production") {
-                $admin->sendMail($farmerRow['firstname'], Emailing::FINANCE_APPLICATION_UPDATE, $farmerRow['email'], NULL, NULL, NULL, $farmerRow['created_on']);
+            if ($result) {
+                $result1 = $finance->selectSingleFinanceRegistrationStatusByID($data->finance_application_id);
+    
+                $row["updated_data"] = $result1->fetch(PDO::FETCH_ASSOC);
+    
+                $result2 = $finance->getFarmerEmailAndFirstnameFromFinanceApplicationID($data->finance_application_id);
+                $farmerRow = $result2->fetch(PDO::FETCH_ASSOC);
+    
+                file_put_contents('php://stderr', "\ndate of creation " . $farmerRow['created_on'] . "\n" . "\n", FILE_APPEND | LOCK_EX);
+    
+                // send email
+                if (getenv("CURR_ENV") == "production") {
+                    $admin->sendMail($farmerRow['firstname'], Emailing::FINANCE_APPLICATION_UPDATE, $farmerRow['email'], NULL, NULL, NULL, $farmerRow['created_on']);
+                }
+                
+                http_response_code();
+                echo json_encode($row);
+            } else {
+                http_response_code(400);
             }
-            
-            http_response_code();
-            echo json_encode($row);
         } else {
+            file_put_contents('php://stderr', print_r("\n\n" . 'Bad data provided in updatestatus.php' . "\n", TRUE));
+
             http_response_code(400);
+            echo json_encode(
+                array(
+                    'message' => 'Bad data provided',
+                    'response' => 'NOT OK',
+                    'response_code' => http_response_code()
+                )
+            );
         }
         
 
